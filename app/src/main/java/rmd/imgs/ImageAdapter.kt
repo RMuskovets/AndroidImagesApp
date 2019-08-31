c0b3be0b1024bc6a74002fdb690962b98d400f05
RMuskovets <rmuskovets@gmail.com>
2019-08-31 13:45:00 +0300

    Application

---
+++ app/src/main/java/rmd/imgs/ImageAdapter.kt
@@ -0,0 +1,35 @@
+package rmd.imgs
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+
+class ImageAdapter(private var ctx: Context, private var imgs: List<Drawable>) : BaseAdapter() {
+    override fun getItemId(p: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return imgs.size
+    }
+
+    operator fun get(index: Int): Drawable {
+        return imgs[index]
+    }
+
+    override fun getItem(pos: Int): Any {
+        return imgs[pos]
+    }
+
+    override fun getView(pos: Int, convertView: View?, parent: ViewGroup): View {
+        @Suppress("USELESS_ELVIS")
+        val img = (convertView as ImageView?)?:ImageView(ctx)
+        img.layoutParams = AbsListView.LayoutParams(128, 128)
+        img.scaleType = ImageView.ScaleType.CENTER_CROP
+        img.setImageDrawable(imgs[pos])
+
+        return img
+    }
+}
---
+++ app/src/main/java/rmd/imgs/MainActivity.kt
@@ -0,0 +1,51 @@
+package rmd.imgs
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.GridView
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var grid: GridView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        grid = findViewById(R.id.gridview)
+        Async().execute("http://placehold.it/128x128", grid, this as Context)
+    }
+
+    class Async : AsyncTask<Any, Int, Bitmap>() {
+
+        @SuppressLint("StaticFieldLeak")
+        private lateinit var grid: GridView
+        @SuppressLint("StaticFieldLeak")
+        private lateinit var ctx: Context
+
+        private fun url2bitmap(imgurl: String): Bitmap {
+            val res: Bitmap
+            val url: URL? = URL(imgurl)
+            res = BitmapFactory.decodeStream(url?.openStream())
+            return res
+        }
+
+        override fun doInBackground(vararg args: Any): Bitmap {
+            val url = args[0] as String
+            grid = args[1] as GridView
+            ctx = args[2] as Context
+            return url2bitmap(url)
+        }
+
+        override fun onPostExecute(result: Bitmap) {
+            val bitmaps = List(10) { BitmapDrawable(result) }
+            grid.adapter = ImageAdapter(ctx, bitmaps)
+        }
+    }
+}
