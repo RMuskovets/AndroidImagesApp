3bfb6b42796fefadc800a0c4fbc50733961ee318
RMuskovets <rmuskovets@gmail.com>
2019-10-12 19:09:14 +0300

    Update ImageAdapter.kt

--- app/src/main/java/rmd/imgs/ImageAdapter.kt
+++ app/src/main/java/rmd/imgs/ImageAdapter.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ImageView
+import android.widget.AbsListView
+import android.widget.BaseAdapter
 
 class ImageAdapter(private var ctx: Context, private var imgs: List<Drawable>) : BaseAdapter() {
     override fun getItemId(p: Int): Long {
