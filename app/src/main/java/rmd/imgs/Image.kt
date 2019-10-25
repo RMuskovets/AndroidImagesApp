14122ed10ad952b3a44ff38eaee7744d4a0931b7
RMuskovets <rmuskovets@gmail.com>
2019-10-25 20:40:32 +0300

    working now)

---
+++ app/src/main/java/rmd/imgs/Image.kt
@@ -0,0 +1,5 @@
+package rmd.imgs
+
+import android.graphics.drawable.Drawable
+
+data class Image(val img: Drawable, var isShown: Boolean)
--- app/src/main/java/rmd/imgs/ImageAdapter.kt
+++ app/src/main/java/rmd/imgs/ImageAdapter.kt
@@ -1,12 +1,17 @@
 package rmd.imgs
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import kotlin.random.Random
 
-class ImageAdapter(private var ctx: Context, private var imgs: List<Drawable>) : BaseAdapter() {
+class ImageAdapter(private var ctx: Activity, private var imgs: MutableList<Image>) : BaseAdapter() {
+
+    private var parent: ViewGroup = ctx.findViewById(R.id.gridview)
+
     override fun getItemId(p: Int): Long {
         return 0
     }
@@ -15,7 +20,7 @@ class ImageAdapter(private var ctx: Context, private var imgs: List<Drawable>) :
         return ctx.resources.getInteger(R.integer.tbl_sz_w) * ctx.resources.getInteger(R.integer.tbl_sz_h)
     }
     operator fun get(index: Int): Drawable {
-        return imgs[index]
+        return imgs[index].img
     }
 
     override fun getItem(pos: Int): Any {
@@ -27,8 +32,28 @@ class ImageAdapter(private var ctx: Context, private var imgs: List<Drawable>) :
         val img = (convertView as ImageView?)?:ImageView(ctx)
         img.layoutParams = AbsListView.LayoutParams(128, 128)
         img.scaleType = ImageView.ScaleType.CENTER_CROP
-        img.setImageDrawable(imgs[pos])
-
+        if (imgs[pos].isShown) {
+            img.setImageDrawable(imgs[pos].img)
+        } else {
+            img.setImageDrawable(null)
+        }
         return img
+    }
+
+    fun random() {
+        val pos = Random.nextInt(count)
+        for (i in IntRange(0, imgs.size-1)) {
+            imgs[i] = Image(imgs[i].img, pos == i)
+        }
+        refresh()
+    }
+
+    private fun refresh() {
+//        try {
+//            parent.invalidate()
+//        } catch (e: Throwable) {
+//            parent.postInvalidate()
+//        }
+        this.notifyDataSetChanged()
     }
 }
